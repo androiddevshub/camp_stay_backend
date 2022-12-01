0fb92c22abe4992f1a5e7a3f607917eb5cf5e839
Shubham jain <shubhamjain@192.168.1.6>
2022-12-01 16:25:10 +0530

    add user apis

---
+++ app/api/tests.rb
@@ -0,0 +1,10 @@
+class Tests < Api
+  format :json
+  default_format :json
+
+  namespace :tests, desc: "Tests Related Operations" do
+    get "/" do
+      { status: true, message: "I am here in the test api" }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'components#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  mount Api => "/"
   # Defines the root path route ("/")
   # root "articles#index"
 end
